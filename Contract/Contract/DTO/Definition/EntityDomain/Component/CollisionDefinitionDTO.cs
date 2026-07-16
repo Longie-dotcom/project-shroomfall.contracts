ab32375b3b1cd8ab9159702f8ca55f82f827bdd3
Long Dong <longdong32120@gmail.com>
2026-07-16 14:18:01 +0700

    update: add collision role

--- Contract/Contract/DTO/Definition/EntityDomain/Component/CollisionDefinitionDTO.cs
+++ Contract/Contract/DTO/Definition/EntityDomain/Component/CollisionDefinitionDTO.cs
@@ -13,12 +13,23 @@ namespace Contract.DTO.Definition.EntityDomain.Component
     {
         public CollisionDefinitionDTO() { ComponentType = nameof(CollisionDefinitionDTO); }
 
+#if NET9_0
+        [TsIgnore]
+#endif
+        public CollisionRole? CollisionRole { get; set; }
+
         public CollisionShapeType ShapeType { get; set; }
         public float Width { get; set; }
         public float Height { get; set; }
         public float Radius { get; set; }
         public bool IsBlocking { get; set; }
+#if NET9_0
+        [TsIgnore]
+#endif
         public CollisionLayer Layer { get; set; }
+#if NET9_0
+        [TsIgnore]
+#endif
         public CollisionLayer Mask { get; set; }
         public float OffsetX { get; set; }
         public float OffsetY { get; set; }
---
+++ Contract/Contract/Enum/EntityDomain/CollisionRole.cs
@@ -0,0 +1,21 @@
+﻿#if NET9_0
+using TypeGen.Core.TypeAnnotations;
+#endif
+
+namespace Contract.Enum.EntityDomain
+{
+#if NET9_0
+    [ExportTsEnum(OutputDir = "enum/entity-domain")]
+#endif
+    public enum CollisionRole
+    {
+        Player,
+        Enemy,
+
+        PlayerProjectile,
+        EnemyProjectile,
+
+        Collectible,
+        Wall,
+    }
+}
