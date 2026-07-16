6368ec74a5497518f380a1d11e8a415241b58c2f
Long Dong <longdong32120@gmail.com>
2026-07-16 21:32:46 +0700

    update: add admin api to fetch running room

---
+++ Contract/Contract/DTO/Feature/Admin/Response/RoomInstanceDTO.cs
@@ -0,0 +1,18 @@
+﻿#if NET9_0
+using TypeGen.Core.TypeAnnotations;
+#endif
+using Contract.DTO.Runtime.EntityDomain;
+using Contract.DTO.Runtime.WorldDomain;
+using System.Collections.Generic;
+
+namespace Contract.DTO.Feature.Admin.Response
+{
+#if NET9_0
+    [ExportTsInterface(OutputDir = "dto/feature/admin/response")]
+#endif
+    public class RoomInstanceDTO
+    {
+        public RoomSpatialDTO Room { get; set; } = new RoomSpatialDTO();
+        public List<EntityInstanceDTO> Entities = new List<EntityInstanceDTO>();
+    }
+}
