6a42d08ab2dd5303cac85bc6e445667a0fec9317
Long Dong <longdong32120@gmail.com>
2026-07-16 21:17:38 +0700

    update

---
+++ Contract/Contract/DTO/Feature/Game/Response/InventoryItemChangedDTO.cs
@@ -0,0 +1,18 @@
+﻿#if NET9_0
+using TypeGen.Core.TypeAnnotations;
+#endif
+
+using Contract.DTO.Runtime.MetaDomain;
+using Contract.Enum.MetaDomain.Item;
+
+namespace Contract.DTO.Feature.Game.Response
+{
+#if NET9_0
+    [ExportTsInterface(OutputDir = "dto/feature/game/response")]
+#endif
+    public class InventoryItemChangedDTO
+    {
+        public ItemInstanceDTO Item { get; set; } = new ItemInstanceDTO();
+        public ItemInventorySyncEvent EventType { get; set; }
+    }
+}
