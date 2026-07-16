15cc1423e4020027408161a4bedcee3c251d09c3
Long Dong <longdong32120@gmail.com>
2026-07-16 16:19:27 +0700

    add: query dto for room

---
+++ Contract/Contract/DTO/Feature/Design/Command/RoomDefinitionQueryDTO.cs
@@ -0,0 +1,18 @@
+﻿using Contract.Enum.WorldDomain;
+#if NET9_0
+using TypeGen.Core.TypeAnnotations;
+#endif
+
+namespace Contract.DTO.Feature.Design.Command
+{
+#if NET9_0
+    [ExportTsInterface(OutputDir = "dto/feature/design/command")]
+#endif
+    public class RoomDefinitionQueryDTO
+    {
+        public string? SearchTerm { get; set; }
+        public RoomType? Type { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
