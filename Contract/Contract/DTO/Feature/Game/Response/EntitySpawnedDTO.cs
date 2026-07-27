ce379ec0499fbd419eed8f5b64d278e3f98a2d0a
Long Dong <longdong32120@gmail.com>
2026-07-27 23:50:40 +0700

    add: spawn dto

---
+++ Contract/Contract/DTO/Feature/Game/Response/EntitySpawnedDTO.cs
@@ -0,0 +1,17 @@
+﻿using Contract.DTO.Common;
+using Contract.DTO.Runtime.EntityDomain;
+#if NET9_0
+using TypeGen.Core.TypeAnnotations;
+#endif
+
+namespace Contract.DTO.Feature.Game.Response
+{
+#if NET9_0
+    [ExportTsInterface(OutputDir = "dto/feature/game/response")]
+#endif
+    public class EntitySpawnedDTO
+    {
+        public EntityInstanceDTO EntityInstance { get; set; } = new EntityInstanceDTO();
+        public Vector2DTO? Direction { get; set; } 
+    }
+}
